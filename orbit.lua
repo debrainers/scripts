14bc94544ac6ffeecb487df14d29f492cd2dcb42
debrainers <idebrainers@gmail.com>
2026-03-05 10:30:00 +0100

    Update orbit.lua

--- orbit.lua
+++ orbit.lua
@@ -1,5 +1,5 @@
 -- UI ISNT MINE!!! full credits for arcane ui go to "192.0.0.2" on discord!
-loadstring(game:HttpGet("https://github.com/debrainers/scripts/blob/main/ArcaneUiNOTMINE"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/debrainers/scripts/refs/heads/main/ArcaneUiNOTMINE"))()
 repeat wait() until Arcane
 
 local Window = Arcane:CreateWindow("@debrainers", Vector2.new(650, 350), "Default")
